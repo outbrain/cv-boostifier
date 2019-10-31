c03588120d4eadcbd7929dccfb71f210feae1763
tshushan <tshushan@outbrain.com>
2019-10-31 21:48:45 +0200

    move swagger spec to separate file

--- src/components/SwaggerTheme/SwaggerTheme.tsx
+++ src/components/SwaggerTheme/SwaggerTheme.tsx
@@ -2,11 +2,12 @@ import React, {PropsWithChildren, useEffect, useRef} from 'react';
 import './SwaggerTheme.css';
 import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
+import {swaggerSpec} from './swaggerSpec';
 
 export function SwaggerTheme(props: PropsWithChildren<any>) {
   const {profile} = props;
   const generateSpec = (profile: any) => {
-    return {"tags":[{"name":"personal","description":"personal info about me"},{"name":"education","description":"List of institutes"},{"name":"experience","description":"Jobs and Projects"},{"name":"skills","description":"My skills"}],"swagger":"2.0","host":"dummy","basePath":"/","info":{"description":"This is an API for Tsachi CV","version":"1.0.3","title":"CV API"},"paths":{"/personal":{"get":{"tags":["personal"],"summary":"My personal info","description":"Returns an object with my info","operationId":"getPersonalData","produces":["application/json"],"responses":{"200":{"description":"successful operation"}}}},"/education":{"get":{"tags":["education"],"summary":"My education info","description":"Returns an Array of institutes","operationId":"getEducationData","produces":["application/json"],"responses":{"200":{"description":"successful operation"}}}},"/experience":{"get":{"tags":["experience"],"summary":"My experience info","description":"Returns an Array of jobs","operationId":"getExperienceData","produces":["application/json"],"responses":{"200":{"description":"successful operation"}}}},"/skills":{"get":{"tags":["skills"],"summary":"My experience info","description":"Returns an Array of skills","operationId":"getSkillsData","produces":["application/json"],"responses":{"200":{"description":"successful operation"}}}}}};
+    return swaggerSpec;
   };
 
   const requestInterceptor = (req: any) => {
