59c2294d877c48c894b4a487d208f6dbc567eadb
Yonatan Maman <yonatan@outbrain.com>
2019-11-02 23:51:57 +0200

    added Made with Love messge to the index.html and to the console

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,22 @@ import {Settings} from './components/Settings/Settings';
 import {ShareCv} from './components/ShareCv/ShareCv';
 
 const App: React.FC = () => {
+  console.log(`
+  made with ♥ by
+
+  ___        _   _          _
+ / _ \\ _   _| |_| |__  _ __(_) __ _ _ __
+| | | | | | | __| '_ \\| '__| |/ _\` | '_ \\
+| |_| | |_| | |_| |_) | |  | | (_| | | | |
+ \\___/ \\__,_|\\__|_.__/|_|  |_|\\__,_|_| |_|
+
+ _____             _                      _
+| ____|_ __   __ _(_)_ __   ___  ___ _ __(_)_ __   __ _
+|  _| | '_ \\ / _\` | | '_ \\ / _ \\/ _ \\ '__| | '_ \\ / _\` |
+| |___| | | | (_| | | | | |  __/  __/ |  | | | | | (_| |
+|_____|_| |_|\\__, |_|_| |_|\\___|\\___|_|  |_|_| |_|\\__, |
+             |___/                                |___/
+`)
   const searchParams = new URLSearchParams(window.location.search);
   const hideSettings = searchParams.get('hideSettings') === 'true';
   const hideShare = searchParams.get('hideShare') === 'true';
