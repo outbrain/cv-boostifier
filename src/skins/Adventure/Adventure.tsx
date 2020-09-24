989b29d56e291635936aaefd00025248d33c3442
= <blaaarp@gmail.com>
2020-09-25 00:21:11 +0300

    horizontal scrolling levels design

--- src/skins/Adventure/Adventure.tsx
+++ src/skins/Adventure/Adventure.tsx
@@ -6,21 +6,24 @@ export function Adventure(props: PropsWithChildren<IProfileProps>) {
   const {basics, skills, work, education, references, projects, publications, languages} = props.profile;
 
   return (
-    <div className="adventure flex">
+    <div>
       <div className="figure"></div>
-      <div className="basics"></div>
-      <div className="awards"></div>
-      <section className="education"></section>
-      <section className="interests"></section>
-      <section className="languages"></section>
-      <section className="meta"></section>
-      <section className="projects"></section>
-      <section className="publications"></section>
-      <section className="references"></section>
-      <section className="skills"></section>
-      <section className="volunteer"></section>
-      <section className="work"></section>
-
+      <div className="adventure outer-wrapper">
+        <div className="wrapper">
+          <section className="lvl-basics slide one"></section>
+          <section className="lvl-awards slide"></section>
+          <section className="lvl-education slide"></section>
+          <section className="lvl-interests slide"></section>
+          {/*<section className="languages"></section>*/}
+          {/*<section className="meta"></section>*/}
+          {/*<section className="projects"></section>*/}
+          {/*<section className="publications"></section>*/}
+          {/*<section className="references"></section>*/}
+          {/*<section className="skills"></section>*/}
+          {/*<section className="volunteer"></section>*/}
+          {/*<section className="work"></section>*/}
+        </div>
+      </div>
     </div>
   );
 }
