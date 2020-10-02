5fa9e6504af40e0c1fc39a089e2b5118f7f8e896
yonatan ben avraham <yonbav@gmail.com>
2020-10-02 14:07:11 +0300

    small fixes for city-backgrounds

--- src/skins/Adventure/components/Education/EducationComponent.tsx
+++ src/skins/Adventure/components/Education/EducationComponent.tsx
@@ -7,25 +7,23 @@ export function EducationComponent(props: {
   scrollLeft: number;
   scrollTop: number;
 }) {
-  const getEducationDataDiv = (institution: Education) => {
-    return <div className="lvl-education-data">
-      {Object.entries(institution).map(data => <div><b>{data[0]}</b>: {data[1]}</div>)}
-    </div>
+  const getEducationDataDiv = (education: Education[]) => {
+    return education.map((institution, index) => 
+      <div className="lvl-education-data" key={`education-${index}`} >
+        {Object.entries(institution).map((data, index) => <div key={`data-${index}`}><b>{data[0]}</b>: {data[1]}</div>)}
+      </div>
+    )
   }
 
   return (
     <div className="lvl-education-background" style={{width:`${props.education.length*100}vw`}}>
+      <div className="lvl-education-item lvl-education-road"/>
+      <div className="lvl-education-item lvl-education-houses-1"/>
+      <div className="lvl-education-item lvl-education-houses-2"/>
+      <div className="lvl-education-item lvl-education-houses-3"/>
+      <div className="lvl-education-item lvl-education-crosswalk"/>
       {
-        props.education.map((institution, index) => 
-        <div key={`education-${index}`} >
-          {getEducationDataDiv(institution)}
-          <div className="lvl-education-item lvl-education-road"/>
-          <div className="lvl-education-item lvl-education-houses-1"/>
-          <div className="lvl-education-item lvl-education-houses-2"/>
-          <div className="lvl-education-item lvl-education-houses-3"/>
-          <div className="lvl-education-item lvl-education-crosswalk"/>
-        </div>
-        )
+        getEducationDataDiv(props.education)
       }
     </div>
   );
--- src/skins/Adventure/components/References/ReferencesComponent.tsx
+++ src/skins/Adventure/components/References/ReferencesComponent.tsx
@@ -7,25 +7,23 @@ export function ReferencesComponent(props: {
   scrollLeft: number;
   scrollTop: number;
 }) {
-  const getReferencesDataDiv = (reference: Reference) => {
-    return <div className="lvl-references-data">
-      {Object.entries(reference).map(data => <div><b>{data[0]}</b>: {data[1]}</div>)}
-    </div>
+  const getReferencesDataDiv = (references: Reference[]) => {
+    return references.map((reference, index) => 
+      <div className="lvl-references-data" key={`reference-${index}`} >
+        {Object.entries(reference).map((data,index) => <div key={`data-${index}`}><b>{data[0]}</b>: {data[1]}</div>)}
+      </div>
+    )
   }
   return (
     <div className="lvl-references-background" style={{width:`${props.references.length*100}vw`}}>
+      <div className="lvl-references-item lvl-references-road"></div>
+      <div className="lvl-references-item lvl-references-houses-1"></div>
+      <div className="lvl-references-item lvl-references-houses-2"></div>
+      <div className="lvl-references-item lvl-references-houses-3"></div>
+      <div className="lvl-references-item lvl-references-fountain"></div>
+      <div className="lvl-references-item lvl-references-decortions"></div>
       {
-        props.references.map((reference, index) => 
-        <div key={`reference-${index}`} >
-          {getReferencesDataDiv(reference)}
-          <div className="lvl-references-item lvl-references-road"></div>
-          <div className="lvl-references-item lvl-references-houses-1"></div>
-          <div className="lvl-references-item lvl-references-houses-2"></div>
-          <div className="lvl-references-item lvl-references-houses-3"></div>
-          <div className="lvl-references-item lvl-references-fountain"></div>
-          <div className="lvl-references-item lvl-references-decortions"></div>
-        </div>
-        )
+        getReferencesDataDiv(props.references)
       }
     </div>
   );
