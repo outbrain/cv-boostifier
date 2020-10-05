a449ba9a56db6f7d8dc0afeef1640200d3bfec24
Katja Golobic <katja.golobic@gmail.com>
2020-10-05 13:14:02 +0200

    Add working canvas demo

--- src/skins/ReturnOfTheCV/ReturnOfTheCV.tsx
+++ src/skins/ReturnOfTheCV/ReturnOfTheCV.tsx
@@ -1,13 +1,38 @@
-import React, {PropsWithChildren} from 'react';
-import './ReturnOfTheCV.scss';
-import {IProfileProps} from '../../models';
+import React, { PropsWithChildren } from "react";
+import "./ReturnOfTheCV.scss";
+import { IProfileProps } from "../../models";
 
 export function ReturnOfTheCV(props: PropsWithChildren<IProfileProps>) {
-  const {basics, skills, work, education, references, projects, publications, languages} = props.profile;
-  
+  // const {
+  //   basics,
+  //   skills,
+  //   work,
+  //   education,
+  //   references,
+  //   projects,
+  //   publications,
+  //   languages,
+  // } = props.profile;
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+
+  React.useEffect(() => {
+    if (canvasRef.current) {
+      const canvasContext = canvasRef.current?.getContext("2d");
+      if (canvasContext) {
+        canvasContext.beginPath();
+        canvasContext.arc(50, 50, 50, 0, 2 * Math.PI);
+        canvasContext.fill();
+      }
+    }
+  });
+
   return (
     <>
-      <div>Hello world</div>
+      <canvas
+        ref={canvasRef}
+        width={window.innerWidth}
+        height={window.innerHeight}
+      />
     </>
   );
 }
