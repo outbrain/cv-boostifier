cdacf3770cacb5b6a59b6cb22f18136d00a0ffbf
Katja Golobic <katja.golobic@gmail.com>
2020-10-05 15:40:32 +0200

    Add text

--- src/skins/ReturnOfTheCV/ReturnOfTheCV.tsx
+++ src/skins/ReturnOfTheCV/ReturnOfTheCV.tsx
@@ -3,16 +3,16 @@ import "./ReturnOfTheCV.scss";
 import { IProfileProps } from "../../models";
 
 export function ReturnOfTheCV(props: PropsWithChildren<IProfileProps>) {
-  // const {
-  //   basics,
-  //   skills,
-  //   work,
-  //   education,
-  //   references,
-  //   projects,
-  //   publications,
-  //   languages,
-  // } = props.profile;
+  const {
+    basics,
+    skills,
+    work,
+    education,
+    references,
+    projects,
+    publications,
+    languages,
+  } = props.profile;
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
 
   React.useEffect(() => {
@@ -142,7 +142,7 @@ export function ReturnOfTheCV(props: PropsWithChildren<IProfileProps>) {
   });
 
   return (
-    <>
+    <div className="return-of-the-cv-skin">
       <canvas
         ref={canvasRef}
         className="stretch"
@@ -152,21 +152,166 @@ export function ReturnOfTheCV(props: PropsWithChildren<IProfileProps>) {
       <div id="crawl-container" className="stretch">
         <div id="crawl">
           <div id="crawl-content">
-            <h1>Episode IV</h1>
-            <h2>A NEW HOPE</h2>
-            <p>
-              It is a period of civil war. Rebel spaceships, striking from a
-              hidden base, have won their first victory against the evil
-              Galactic Empire.
-            </p>
-            <p>
-              During the battle, rebel spies managed to steal secret plans to
-              the Empire’s ultimate weapon, the DEATH STAR, an armored space
-              station with enough power to destroy an entire planet.
-            </p>
+            <h1>{basics?.name}</h1>
+            <h2>{basics?.label}</h2>
+            <p>{basics?.summary}</p>
+            <section>
+              <h3>Details</h3>
+              <p>
+                {basics?.location?.address && `${basics?.location?.address}, `}
+                {basics?.location?.city && `${basics?.location?.city}, `}
+                {basics?.location?.countryCode && basics?.location?.countryCode}
+              </p>
+              <p>{basics?.phone}</p>
+              <p>{basics?.email}</p>
+            </section>
+            {(skills || []).length > 0 && (
+              <section>
+                <h3>Skills</h3>
+                <p className="skills">
+                  {(skills || []).map((skill) => {
+                    return <span key={skill.name}>{skill.name}</span>;
+                  })}
+                </p>
+              </section>
+            )}
+            {basics?.summary && (
+              <section>
+                <h2>Profile</h2>
+                <p>{basics?.summary}</p>
+              </section>
+            )}
+            {(work || []).length > 0 && (
+              <section>
+                <h2>Experience</h2>
+                {(work || []).map((workItem, ix) => {
+                  return (
+                    <article key={ix}>
+                      <hgroup>
+                        <h4>
+                          {workItem.position}
+                          {workItem.position && workItem.name ? ", " : ""}
+                          {workItem.name}
+                        </h4>
+                        <h6>
+                          {workItem.startDate} - {workItem.endDate}
+                        </h6>
+                      </hgroup>
+                      <p>{workItem.summary}</p>
+                      <ul>
+                        {(workItem.highlights || []).map((highlight) => {
+                          return <li key={highlight}>{highlight}</li>;
+                        })}
+                      </ul>
+                    </article>
+                  );
+                })}
+              </section>
+            )}
+            {(projects || []).length > 0 && (
+              <section>
+                <h2>Projects</h2>
+                {(projects || []).map((project) => {
+                  return (
+                    <article>
+                      <hgroup>
+                        <h4>
+                          {project.name}, {project.entity}
+                        </h4>
+                        <h6>
+                          {project.startDate} - {project.endDate}
+                        </h6>
+                      </hgroup>
+                      <p>{project.description}</p>
+                      <ul>
+                        {(project.highlights || []).map((highlight) => {
+                          return <li>{highlight}</li>;
+                        })}
+                      </ul>
+                    </article>
+                  );
+                })}
+              </section>
+            )}
+            {(education || []).length > 0 && (
+              <section>
+                <h2>Education</h2>
+                {(education || []).map((educationItem, ix) => {
+                  return (
+                    <article key={ix}>
+                      <hgroup>
+                        <h4>
+                          {educationItem.studyType}, {educationItem.area},{" "}
+                          {educationItem.institution}
+                        </h4>
+                        <h6>
+                          {educationItem.startDate} - {educationItem.endDate}
+                        </h6>
+                      </hgroup>
+                      <ul>
+                        {(educationItem.courses || []).map((course) => {
+                          return <li key={course}>{course}</li>;
+                        })}
+                      </ul>
+                    </article>
+                  );
+                })}
+              </section>
+            )}
+            {(languages || []).length > 0 && (
+              <section>
+                <h2>Languages</h2>
+                <ul>
+                  {(languages || []).map((language) => {
+                    return (
+                      <li key={language.language}>
+                        {language.language} - {language.fluency}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </section>
+            )}
+            {(references || []).length > 0 && (
+              <section>
+                <h2>References</h2>
+                <ul>
+                  {(references || []).map((reference) => {
+                    return (
+                      <li key={reference.name}>
+                        <h4>{reference.name}</h4>
+                        <p>{reference.reference}</p>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </section>
+            )}
+            {(publications || []).length > 0 && (
+              <section>
+                <h2>Publications</h2>
+                <ul>
+                  {(publications || []).map((publication, ix) => {
+                    return (
+                      <li key={ix}>
+                        <h4>
+                          {publication.name}, {publication.publisher}
+                        </h4>
+                        <p>{publication.summary}</p>
+                        <p>
+                          <a href={publication.website}>
+                            {publication.website}
+                          </a>
+                        </p>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </section>
+            )}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
