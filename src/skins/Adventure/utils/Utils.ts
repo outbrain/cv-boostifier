84ac37ef70aecd3fed9d96de5aae17fccfc1a4a5
Ohad Shivek <oshivek@outbrain.com>
2020-10-05 20:55:37 +0300

    work design

--- src/skins/Adventure/utils/Utils.ts
+++ src/skins/Adventure/utils/Utils.ts
@@ -1,6 +1,34 @@
 export class Utils {
   constructor() {}
-  moveElement = (startingPosition: number, scrollPosition: number,speedRatio: number) => startingPosition - speedRatio * scrollPosition;
+  monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  moveElement = (
+    startingPosition: number,
+    scrollPosition: number,
+    speedRatio: number
+  ) => startingPosition - speedRatio * scrollPosition;
+
+  getMonthName(date: Date): string {
+    return this.monthNames[date.getDate()];
+  }
+
+  formatDate(dateStr: string = ""): string {
+    const date = new Date(dateStr);
+    return `${this.getMonthName(date)} ${date.getFullYear()}`;
+  }
 }
 
-export let utils = new Utils()
+export let utils = new Utils();
