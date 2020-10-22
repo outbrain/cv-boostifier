8fb6cc01ab1f785a7d88aa72868df957999506ef
= <danielg@outbrain.com>
2020-10-22 23:06:31 +0300

    cloud fix+ static floor+ boxes cosmetics + parallax mountains bit slower

--- src/skins/Adventure/components/Floor/Floor.tsx
+++ src/skins/Adventure/components/Floor/Floor.tsx
@@ -4,16 +4,7 @@ import { utils } from "../../utils/Utils";
 export function Floor(props: { worldSize: number,scrollLeft:number }) {
   return (
     <div
-      className="floor"
-      style={{
-        width: `${props.worldSize}px`,
-        backgroundPositionX: utils.moveElement(
-          0,
-          props.scrollLeft,
-          -0.9
-        ),
-      }}
-    >
+      className="floor">
       <div className="ramp-general">
         <div className="ramp-part-1" />
         <div className="ramp-part-2" />
