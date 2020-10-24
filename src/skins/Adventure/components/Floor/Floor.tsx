69ccb6739d9db2a88d66cdfd26fe45ef1444c83a
ybenavraham <ybenavraham@outbrain.com>
2020-10-24 12:43:55 +0300

    fixing the floor

--- src/skins/Adventure/components/Floor/Floor.tsx
+++ src/skins/Adventure/components/Floor/Floor.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { utils } from "../../utils/Utils";
 
-export function Floor(props: { worldSize: number }) {
+export function Floor(props: { worldSize: number, scrollLeft:number}) {
   return (
-    <div className="floor">
+    <div
+      className="floor"
+      style={props.scrollLeft !== 0 ? {
+        width: `${props.worldSize}px`,
+        backgroundPositionX: utils.moveElement(
+          0,
+          props.scrollLeft,
+          -0.9
+        ),
+      } : {}}>
       <div className="ramp-general">
         <div className="ramp-part-1" />
         <div className="ramp-part-2" />
