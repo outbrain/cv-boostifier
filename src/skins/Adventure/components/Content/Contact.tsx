b05273150ad71e755b29b91dba25b6dfbc1bc54f
Ohad Shivek <oshivek@outbrain.com>
2020-11-03 12:45:17 +0200

    fixes of trees and skills

--- src/skins/Adventure/components/Content/Contact.tsx
+++ src/skins/Adventure/components/Content/Contact.tsx
@@ -1,32 +1,57 @@
-import React from 'react';
-import {Basics} from "../../../../models";
-import {utils} from "../../utils/Utils";
-import "./Contact.scss"
+import React from "react";
+import { Basics } from "../../../../models";
+import { utils } from "../../utils/Utils";
+import "./Contact.scss";
 
-export function ContentComponent(props: { data: Basics, screenSize: number, scrollLeft: number, scrollTop: number }) {
-  const MOUNTS_MOVEMENT_RATIO = 0.1;
-  const MOUNTS_STARTING_OFFSET = 0.9;
+export function ContentComponent(props: {
+  data: Basics;
+  screenSize: number;
+  scrollLeft: number;
+  scrollTop: number;
+}) {
+  const MOUNTS_MOVEMENT_RATIO = 0.2;
+  const MOUNTS_STARTING_OFFSET = 0.3;
 
   return (
-
     <div className="lvl-contact">
-      <div>
-      </div>
-      <div className=" ground-night" >
-        <div className="wooden-ground-night" style={{backgroundPositionX: utils.moveElement(props.screenSize * MOUNTS_MOVEMENT_RATIO, props.scrollLeft, 0.2)}}>
-        <div className="wooden-ground-night-2"
-               style={{backgroundPositionX: utils.moveElement(props.screenSize * MOUNTS_STARTING_OFFSET, props.scrollLeft, 0.2)}}></div>
+      <div></div>
+      <div className=" ground-night">
+        <div
+          className="wooden-ground-night"
+          style={{
+            backgroundPositionX: utils.moveElement(
+              props.screenSize * MOUNTS_STARTING_OFFSET,
+              props.scrollLeft,
+              MOUNTS_MOVEMENT_RATIO
+            ),
+          }}
+        >
+          <div
+            className="wooden-ground-night-2"
+            style={{
+              backgroundPositionX: utils.moveElement(
+                props.screenSize * MOUNTS_STARTING_OFFSET,
+                props.scrollLeft,
+                MOUNTS_MOVEMENT_RATIO
+              ),
+            }}
+          ></div>
         </div>
       </div>
-      <div className="heading contact">
-
-      </div>
+      <div className="heading contact"></div>
       <div className="heading contact-list">
-          <h1>Contact</h1>
-          {props.data.picture &&
+        <h1>Contact</h1>
+        {props.data.picture && (
           <div>
-              <img className="user-image" src={props.data.picture} height={150} width={150} alt={props.data.name}/>
-          </div>}
+            <img
+              className="user-image"
+              src={props.data.picture}
+              height={150}
+              width={150}
+              alt={props.data.name}
+            />
+          </div>
+        )}
         <div className="details">
           <ul>
             <li className="skill-item">{props.data?.name}</li>
@@ -34,14 +59,20 @@ export function ContentComponent(props: { data: Basics, screenSize: number, scro
             <li className="skill-item">{props.data?.phone}</li>
             <li className="skill-item">{props.data?.url}</li>
           </ul>
-        <div>{props.data.profiles && props.data.profiles.length > 0 &&
-        props.data.profiles.map((profile, index) => {
-          return <div className="link-item" key={index}><a href={profile.url}>{profile.network}</a></div>
-        })
-        }</div>
+          <div>
+            {props.data.profiles &&
+              props.data.profiles.length > 0 &&
+              props.data.profiles.map((profile, index) => {
+                return (
+                  <div className="link-item" key={index}>
+                    <a href={profile.url}>{profile.network}</a>
+                  </div>
+                );
+              })}
+          </div>
         </div>
       </div>
-      <div className="cabin"/>
+      <div className="trees-trick" />
       <div className="rock"></div>
     </div>
   );
